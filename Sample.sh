1db1870c88a9aaebae922359e496c3c4e7574024
EC2 Default User <ec2-user@ip-172-31-44-57.us-east-2.compute.internal>
2019-02-22 14:56:48 +0000

    intial commit

---
+++ Sample.sh
@@ -0,0 +1,12 @@
+#!/nin/bash
+echo "This is Shell sample Program"
+name="Ravindra Reddy M"
+set -x
+id=12204
+echo "Detals are:"
+echo "Name is:"$name
+set +x
+echo "Id is:"$id
+echo "Details are Displayed"
+echo "Best Deels"
+date
